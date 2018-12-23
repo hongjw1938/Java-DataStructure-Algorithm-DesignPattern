7114f26d9435f06986a9525c1b7f447755487afa
Hong JW <sighingtree@naver.com>
2018-12-23 03:43:32 +0000

    merge, shell sort add

--- Algorithm/Sort_algorithm/Sort_algorithms.java
+++ Algorithm/Sort_algorithm/Sort_algorithms.java
@@ -6,11 +6,13 @@ public class Sort_algorithms {
         int[] array1 = { 20, 35, -15, 7, 55, 1, -22};
         int[] array2 = {4725, 4586, 1330, 8792, 1594, 5729};
         int[] array3 = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
-        insertionSort2(array1);
+        int[] array4 = { 10, -12, -52, 2315, 23, -5, 1, 0, 2, 7312};
+        shellSort2(array1);
+        mergeSort(array4, 0, array4.length);
 
 
-        for(int i=0; i < array1.length; i++){
-            System.out.println(array1[i]);
+        for(int i=0; i < array4.length; i++){
+            System.out.println(array4[i]);
         }
     }
 
@@ -96,5 +98,61 @@ public class Sort_algorithms {
             }
             array[j] = tobeinserted;
         }
+    }
+
+    // Shell 정렬 좌측부터 정렬
+    public static void shellSort(int[] array){
+        for(int gap = array.length / 2; gap > 0; gap /= 2){
+            for(int i=gap; i < array.length; i++){
+                int tobeInserted = array[i];
+                int j=i;
+                while(j >= gap && array[j-gap] > tobeInserted){
+                    array[j] = array[j-gap];
+                    j -= gap;
+                }
+                array[j] = tobeInserted;
+            }
+        }
+    }
+
+    // Shell 정렬 우측부터 정렬
+    public static void shellSort2(int[] array){
+        for(int gap = array.length / 2; gap > 0; gap /= 2){
+            for(int i=array.length-gap-1; i >= 0; i--){
+                int tobeInserted = array[i];
+                int j=i;
+                while(j < array.length-gap && array[j+gap] < tobeInserted ){
+                    array[j] = array[j+gap];
+                    j += gap;
+                }
+                array[j] = tobeInserted;
+            }
+        }
+    }
+
+    // Merge(병합) 정렬
+    public static void mergeSort(int[] array, int start, int end){
+        if(end - start < 2) return;
+
+        int mid = (start + end) / 2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid, end);
+        merging(array, start, mid, end);
+    }
+
+    public static void merging(int[] array, int start, int mid, int end){
+        if(array[mid-1] <= array[mid]) return;
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+        int[] temp = new int[end - start];
+
+        while(i < mid && j < end){
+            temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+
+        System.arraycopy(temp, 0, array, start, tempIndex);
     }
 }
