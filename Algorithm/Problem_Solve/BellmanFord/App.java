83e73ba8d9958d40814e1c6e6a4f8bf390a54738
Hong JW <sighingtree@naver.com>
2019-01-31 11:43:19 +0000

    bellman ford imple

--- Algorithm/Problem_Solve/BellmanFord/App.java
+++ Algorithm/Problem_Solve/BellmanFord/App.java
@@ -1,4 +1,25 @@
 package com.problemsolve.bellman_ford;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+
+    public static void main(String[] args){
+        List<Vertex> vertexList = new ArrayList<>();
+
+        vertexList.add(new Vertex("A"));
+        vertexList.add(new Vertex("B"));
+        vertexList.add(new Vertex("C"));
+
+        List<Edge> edgeList = new ArrayList<>();
+
+        edgeList.add(new Edge(1, vertexList.get(0), vertexList.get(1)));
+        edgeList.add(new Edge(-11, vertexList.get(1), vertexList.get(2)));
+        edgeList.add(new Edge(-1, vertexList.get(2), vertexList.get(1)));
+
+        BellmanFord bellmanFord = new BellmanFord(edgeList, vertexList);
+        bellmanFord.bellmanFord(vertexList.get(0));
+//        bellmanFord.shortestPathTo(vertexList.get(2));
+    }
 }
