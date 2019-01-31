c8ccba5e25c0e439490dbb94762ddfa3adf534a2
Hong JW <sighingtree@naver.com>
2019-01-31 11:53:42 +0000

    bellman ford comment

--- Algorithm/Problem_Solve/BellmanFord/BellmanFord.java
+++ Algorithm/Problem_Solve/BellmanFord/BellmanFord.java
@@ -13,21 +13,27 @@ public class BellmanFord {
     }
 
     public void bellmanFord(Vertex startVertex){
+        // Start에서의 Start의 거리는 0
         startVertex.setDistance(0);
 
         // Relax all Edges of V-1 Iteration
+        // 전체 Vertex에 대해서 바로 실행
         for(int i=0; i < vertexList.size()-1; i++){
+            // 각 Vertex가 연결된 edge를 V-1 번 Relax시킴
             for(Edge edge : edgeList){
 
                 Vertex u = edge.getStartVertex();
                 Vertex v = edge.getTargetVertex();
 
+                // 최대값인 경우 해당 Vertex와는 연결이 안된다는 것.
                 if(u.getDistance() == Double.MAX_VALUE){
                     continue;
                 }
 
+                // 해당 거리와 간선의 cost를 더함
                 double newDistance = u.getDistance() + edge.getWeight();
 
+                // 기존 cost와 새로이 계산된 cost를 비교하여 더 짧으면 update
                 if(newDistance < v.getDistance()){
 
                     v.setDistance(newDistance);
@@ -36,8 +42,11 @@ public class BellmanFord {
             }
         }
 
+        // 이미 계산이 완료되었으니 다시 한 번 모든 Edge를 탐색
         for(Edge edge : edgeList){  // V-th iteration
+            // 해당 Edge에서 edge에 연결된 start Vertex와의 거리가 최대값인 경우는 고려하지 않음
             if(edge.getStartVertex().getDistance() != Double.MAX_VALUE){
+                // Negative Cycle 검사
                 if(hasCycle(edge)){
                     System.out.println("There has been a negative cycle detected");
                     return;
@@ -61,8 +70,9 @@ public class BellmanFord {
         Vertex actual = targetVertex;
 
         while(actual.getPreviousVertex() != null){
-            System.out.println(actual + " - ");
+            System.out.print(actual + " - ");
             actual = actual.getPreviousVertex();
         }
+        System.out.println(actual);
     }
 }
