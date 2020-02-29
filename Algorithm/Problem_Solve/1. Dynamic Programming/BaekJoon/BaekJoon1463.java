bbc710251c519220c21a92a5d48ba85e1f6b4f13
EC2 Default User <ec2-user@ip-10-0-0-104.ap-southeast-1.compute.internal>
2020-02-29 02:22:11 +0000

    bj 1463 bottom-up

--- Algorithm/Problem_Solve/1. Dynamic Programming/BaekJoon/BaekJoon1463.java
+++ Algorithm/Problem_Solve/1. Dynamic Programming/BaekJoon/BaekJoon1463.java
@@ -11,24 +11,40 @@ public class BaekJoon1463 {
         int X = Integer.parseInt(br.readLine());
 
         // Top-Down 방식 풀이
-        System.out.println(dp(X));
+        //System.out.println(topDown(X));
 
+        // Bottom-up 방식 풀이
+        System.out.println(bottomUp(X));
         br.close();
     }
 
-    private static int dp(int x){
+    private static int bottomUp(int x){
+        min[1] = 0;
+        for(int i=2; i <= x; i++){
+            min[i] = min[i-1] + 1;
+            if(i % 2 == 0 && min[i] > min[i/2] + 1){
+                min[i] = min[i/2] + 1;
+            }
+            if(i % 3 == 0 && min[i] > min[i/3] + 1){
+                min[i] = min[i/3] + 1;
+            }
+        }
+        return min[x];
+    }
+
+    private static int topDown(int x){
         if(x == 1){
             return 0;
         } else if(min[x] > 0){
             return min[x];
         }
-        min[x] = dp(x-1) + 1;
+        min[x] = topDown(x-1) + 1;
         if(x % 3 == 0){
-            int temp = dp(x / 3) + 1;
+            int temp = topDown(x / 3) + 1;
             if(min[x] > temp) min[x] = temp;
         }
         if(x % 2 == 0){
-            int temp = dp(x/2) + 1;
+            int temp = topDown(x/2) + 1;
             if(min[x] > temp) min[x] = temp;
         }
         return min[x];
