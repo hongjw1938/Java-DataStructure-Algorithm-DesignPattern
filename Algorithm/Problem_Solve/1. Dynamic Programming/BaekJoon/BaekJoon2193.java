affa20685a50b2423781241825b6b522c4dd841f
Jongwoo Hong <hongjw1938@gmail.com>
2020-03-03 00:54:52 +0000

    dp bj 2193

---
+++ Algorithm/Problem_Solve/1. Dynamic Programming/BaekJoon/BaekJoon2193.java
@@ -0,0 +1,35 @@
+package com.problemsolve.dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon10844 {
+    static int[][] d = new int[101][10];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1; i <= 9; i++){
+            d[1][i] = 1;
+        }
+        for(int i=2; i <= 100; i++){
+            for(int j=0; j <= 9; j++){
+                if(j==0){
+                    d[i][j] += (d[i-1][j+1] % 1000000000);
+                } else if(j==9){
+                    d[i][j] += (d[i-1][j-1] % 1000000000);
+                } else {
+                    d[i][j] += (d[i-1][j-1] + d[i-1][j+1]) % 1000000000;
+                }
+                System.out.print(d[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int ans=0;
+        for(int i=0; i<=9;i++){
+            ans = (ans + d[n][i]) % 1000000000;
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
