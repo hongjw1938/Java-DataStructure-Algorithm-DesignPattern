d2ebad5b71fccdf2733a7b9cb96539deac350815
Jongwoo Hong <hongjw1938@gmail.com>
2020-07-13 14:42:34 +0000

    bf200713

--- DataStructure/Hash_Table/Programmars_1.java
+++ DataStructure/Hash_Table/Programmars_1.java
@@ -3,7 +3,7 @@ package com.datastructure.hashtable;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class Solution {
+public class Programmars_1 {
     public String solution(String[] participant, String[] completion){
         Arrays.sort(participant);
         Arrays.sort(completion);
@@ -43,6 +43,7 @@ public class Solution {
         // keyset의 get은 hash를 이용하나 entry는 해당 집합을 다 가져와서 O(1)이 되기 때문.
         // 물론 keySet()의 get도 그렇지만 같은 O(1)이라도 약간의 차이가 있다고 한다.
         // 참고 : https://stackoverflow.com/questions/3870064/performance-considerations-for-keyset-and-entryset-of-map
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
         for(HashMap.Entry<String, Integer> entry : entries){
             if(entry.getValue() != 0){
                 answer = entry.getKey();
