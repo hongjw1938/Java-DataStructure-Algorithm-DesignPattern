f0d0f2e7f1c930675e4d3d8c3c7dd288eebd8c28
Jongwoo Hong <hongjw1938@gmail.com>
2020-07-18 13:53:38 +0000

    0718

---
+++ DataStructure/Stack/Programmars_4.java
@@ -0,0 +1,46 @@
+package com.datastructure.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+
+        int[] answer = s.solution(progresses, speeds);
+        for(int i=0; i < answer.length; i++){
+            System.out.print(answer[i] + ", ");
+        }
+
+    }
+    public int[] solution(int[] progresses, int[] speeds){
+        int[] complete = new int[progresses.length];
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i < progresses.length; i++){
+            complete[i] = (100 - progresses[i]) % speeds[i] > 0 ? (100 - progresses[i]) / speeds[i] + 1: (100 - progresses[i]) / speeds[i];
+            if(stack.isEmpty() || stack.get(0) >= complete[i]){
+                stack.push(complete[i]);
+            } else {
+                queue.offer(stack.size());
+                while(!stack.isEmpty()){
+                    stack.pop();
+                }
+                stack.push(complete[i]);
+            }
+        }
+        queue.offer(stack.size());
+
+        int answer[] = new int[queue.size()];
+        int index = 0;
+        while(!queue.isEmpty()){
+            answer[index] = queue.poll();
+            index++;
+        }
+        return answer;
+    }
+}
+
